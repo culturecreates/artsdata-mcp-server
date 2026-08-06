40efe5e031c484b7602067051f1795d12d414530
suhail-aliyar <suhail@culturecreates.com>
2026-08-06 14:06:59 +0530

    #4 Added support to search by type(s)

--- app/tool/search_entities.rb
+++ app/tool/search_entities.rb
@@ -2,6 +2,7 @@ require "mcp"
 require "json"
 
 class SearchEntities < MCP::Tool
+
   description "Tool to search for entities in the Artsdata knowledge graph by name and type(optional)."
 
   request_schema_path = File.expand_path("../schema/search_entities_request_schema.json", __dir__)
@@ -14,13 +15,12 @@ class SearchEntities < MCP::Tool
   output_schema(RESPONSE_SCHEMA)
 
   class << self
-    def call(query:, language:, limit:)
+    def call(query:, types: [], language: 'en', limit: 50)
 
-      result = ArtsdataClient.new.search_items(query:, lang: language, limit:)
-      MCP::Tool::Response.new({
-                                type: "text",
-                                text: JSON.generate(result),
-                              }, structured_content: result)
+      result = ArtsdataClient.new.search_items(query:, types: types, lang: language, limit:)
+      MCP::Tool::Response.new(
+        { type: "text", text: JSON.generate(result) },
+        structured_content: result)
     end
   end
 end
