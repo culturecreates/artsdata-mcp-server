e5025bdb45f8bb052710f078509cd04d13addc8c
suhail-aliyar <suhail@culturecreates.com>
2026-09-18 17:13:51 +0400

    Removed swagger integration

--- config/initializers/rswag_api.rb
+++
@@ -1,3 +0,0 @@
-Rswag::Api.configure do |config|
-  config.openapi_root = Rails.root.join("swagger").to_s
-end
--- config/initializers/rswag_ui.rb
+++
@@ -1,3 +0,0 @@
-Rswag::Ui.configure do |config|
-  config.openapi_endpoint "/api-docs/v1/swagger.yaml", "Artsdata MCP API V1"
-end
