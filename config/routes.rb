e0590cbd0893053d67ef08f923214f53ed14f9fb
suhail-aliyar <suhail@culturecreates.com>
2026-09-18 17:14:24 +0400

    cleanup routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
-# config/routes.rb
 Rails.application.routes.draw do
   # health
   get "up" => "rails/health#show", as: :rails_health_check
   # mcp
   mount ->(env) { MyTransport.call(env) }, at: "/mcp"
-
-  get "search-entities", to: "entities#search"
-  # Swagger routes
-  mount Rswag::Ui::Engine => "/api-docs"
-  mount Rswag::Api::Engine => "/api-docs"
-
-
 end
